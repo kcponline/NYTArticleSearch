550830b7855de8292e4ea49c45806f85a7284557
kcponline <kcponline@Admins-MacBook-Pro.local>
2016-08-04 00:35:31 -0700

    commit 02 - check js and jq link

--- logic.js
+++ logic.js
@@ -1,30 +1,37 @@
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch?q="
-+ 'pokemon' + "&api_key=9499cc3db2d441218b9510d399f7e976&limit=10";
+	// pseudo 01: check vjs link
+document.write("js link check - to be comment out later");
+	// pseudo 01: check jq link
+$('#jq-check').html("jq link check - to be comment out later");
 
-        $.ajax({
-                url: queryURL,
-                method: 'GET'
-            })
-            .done(function(response) {
-                var results = response.data;
 
-                for (var i = 0; i < results.length; i++) {
-                    var gifDiv = $('<div class="item">')
 
-                    var rating = results[i].rating;
+// var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch?q="
+// + 'pokemon' + "&api_key=9499cc3db2d441218b9510d399f7e976&limit=10";
 
-                    var p = $('<p>').text("Rating: " + rating);
+//         $.ajax({
+//                 url: queryURL,
+//                 method: 'GET'
+//             })
+//             .done(function(response) {
+//                 var results = response.data;
 
-                    var personImage = $('<img>');
-                    personImage.attr('src', results[i].images.fixed_height.url);
+//                 for (var i = 0; i < results.length; i++) {
+//                     var gifDiv = $('<div class="item">')
 
-                    gifDiv.append(p)
-                    gifDiv.append(personImage)
+//                     var rating = results[i].rating;
 
-                    $('#gifsAppearHere').prepend(gifDiv);
-                }
+//                     var p = $('<p>').text("Rating: " + rating);
 
-            });
+//                     var personImage = $('<img>');
+//                     personImage.attr('src', results[i].images.fixed_height.url);
+
+//                     gifDiv.append(p)
+//                     gifDiv.append(personImage)
+
+//                     $('#gifsAppearHere').prepend(gifDiv);
+//                 }
+
+//             });
 
 
 
